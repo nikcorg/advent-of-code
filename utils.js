178b947b84a437de34b9144b9029cad8a483813a
Niklas Lindgren <niklas@musicglue.com>
2016-12-22 16:57:03 +0000

    feat: add many utils, that are oh so very useful

--- utils.js
+++ utils.js
@@ -36,10 +36,49 @@ const tap =
   m =>
     x => console.log(m) || console.dir(x, { depth: null }) || x;
 
+const split =
+  x =>
+    s => s.split(x);
+
+const join =
+  g =>
+    s => s.join(g);
+
+const map =
+  f =>
+    x => x.map(f);
+
+const reduce =
+  (f, i) =>
+    x => x.reduce(f, i);
+
+const equals =
+  a =>
+    b => a === b;
+
+const not =
+  v => !v;
+
+const lookup =
+  from =>
+    what => from[what];
+
+const constrain =
+  (min, max) =>
+    n => Math.min(max, Math.max(n, min));
+
 module.exports = {
   chain,
   compose,
+  constrain,
+  equals,
+  join,
+  lookup,
+  map,
+  not,
   readFile,
+  reduce,
+  split,
   takeUntilNotNull,
   tap,
   trim
