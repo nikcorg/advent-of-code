c5c18116291b29e46394c17723e9142b7a759db1
Niklas Lindgren <niklas@musicglue.com>
2020-12-06 11:17:14 +0000

    feat: add chunking utility
    group separated by blank lines seems to be a recurring theme, so it
    makes sense to provide a chunking utility

---
+++ 2020/utils/linestream/chunked.go
@@ -0,0 +1,31 @@
+package linestream
+
+type Chunk []*Line
+
+type ChunkedLineChan = chan Chunk
+type ReadOnlyChunkedLineChan = <-chan Chunk
+type WriteOnlyChunkedLineChan = chan<- Chunk
+
+func Chunked(inp ReadOnlyLineChan, out WriteOnlyChunkedLineChan) {
+	var chunk Chunk
+
+	go func() {
+		defer close(out)
+
+		for line := range inp {
+			if line.Content() == "" {
+				if len(chunk) > 0 {
+					out <- chunk
+				}
+				chunk = Chunk{}
+				continue
+			}
+
+			chunk = append(chunk, line)
+		}
+
+		if len(chunk) > 0 {
+			out <- chunk
+		}
+	}()
+}
