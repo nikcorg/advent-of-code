c07b7bac37cd9a44464f49111870305fa16d3c98
Niklas Lindgren <niklas@musicglue.com>
2020-12-10 22:24:24 +0000

    feat: solution for day 10

--- 2020/main.go
+++ 2020/main.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	"github.com/nikcorg/aoc2020/s1"
+	"github.com/nikcorg/aoc2020/s10"
 	"github.com/nikcorg/aoc2020/s2"
 	"github.com/nikcorg/aoc2020/s3"
 	"github.com/nikcorg/aoc2020/s4"
@@ -18,7 +19,7 @@ import (
 	"github.com/nikcorg/aoc2020/s9"
 )
 
-const solved = 9
+const solved = 10
 const inputDir = "_inputs"
 
 type SolverFunc func(io.Reader) error
@@ -94,6 +95,8 @@ func getSolver(ctx context.Context, out io.Writer, puzzle int) Solver {
 		return s8.New(ctx, out)
 	case 9:
 		return s9.New(ctx, out, 25)
+	case 10:
+		return s10.New(ctx, out)
 	default:
 		io.WriteString(os.Stderr, fmt.Sprintf("unknown puzzle: %d\n", puzzle))
 	}
