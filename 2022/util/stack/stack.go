475cc65e6519b2abe6c592e61c2ff1de1313aedb
Niklas Lindgren <niklas@musicglue.com>
2022-12-09 01:18:24 +0000

    refactor(util/stack): add mutex to stack for thread safety

--- 2022/util/stack/stack.go
+++ 2022/util/stack/stack.go
@@ -1,7 +1,10 @@
 package stack
 
+import "sync"
+
 type Stack[T any] struct {
 	stack []T
+	mut   sync.RWMutex
 }
 
 func New[T any]() Stack[T] {
@@ -9,26 +12,45 @@ func New[T any]() Stack[T] {
 }
 
 func (s *Stack[T]) Size() int {
+	s.mut.RLock()
+	defer s.mut.RUnlock()
+
 	return len(s.stack)
 }
 
 func (s *Stack[T]) Push(vs ...T) {
+	s.mut.Lock()
+	defer s.mut.Unlock()
+
 	s.stack = append(vs, s.stack...)
 }
 
 func (s *Stack[T]) Pop() T {
+	s.mut.Lock()
+	defer s.mut.Unlock()
+
 	var v T
-	v, s.stack = s.stack[0], s.stack[1:]
+
+	if len(s.stack) > 0 {
+		v, s.stack = s.stack[0], s.stack[1:]
+	}
+
 	return v
 }
 
 func (s *Stack[T]) PopN(n int) []T {
+	s.mut.Lock()
+	defer s.mut.Unlock()
+
 	var vs []T
 	vs, s.stack = s.stack[0:n], s.stack[n:]
 	return vs
 }
 
 func (s *Stack[T]) Peek() T {
+	s.mut.RLock()
+	defer s.mut.RUnlock()
+
 	var x T
 	if len(s.stack) > 0 {
 		x = s.stack[0]
@@ -37,10 +59,16 @@ func (s *Stack[T]) Peek() T {
 }
 
 func (s *Stack[T]) Clear() {
+	s.mut.Lock()
+	defer s.mut.Unlock()
+
 	s.stack = []T{}
 }
 
 func (s *Stack[T]) Each(f func(T)) {
+	s.mut.RLock()
+	defer s.mut.RUnlock()
+
 	for _, v := range s.stack {
 		f(v)
 	}
